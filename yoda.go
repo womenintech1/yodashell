b81b50f44c4566f0f61eaa42492092db6a8b7c2d
womenintech <109299952+womenintech1@users.noreply.github.com>
2022-09-08 18:20:48 +0200

    Update yoda.go

--- yoda.go
+++ yoda.go
@@ -6,19 +6,15 @@ import (
 	"bufio"
 	"errors"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"os"
 	"os/exec"
 	"strings"
 )
 
 func main() {
-
 	reader := bufio.NewReader(os.Stdin)
 	for {
-		fmt.Print("YoDaShell ------>")
-
+		fmt.Print("Yodashell ---> ")
 		input, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
@@ -30,54 +26,21 @@ func main() {
 	}
 }
 
-var ErrNoPath = errors.New("Pfad erforderlich")
+var ErrNoPath = errors.New("path required")
 
 func execInput(input string) error {
-
 	input = strings.TrimSuffix(input, "\n")
 
 	args := strings.Split(input, " ")
 
 	switch args[0] {
-
 	case "cd":
-
 		if len(args) < 2 {
 			return ErrNoPath
 		}
-
 		return os.Chdir(args[1])
 	case "exit":
-
 		os.Exit(0)
-
-	case "mkdir":
-		err := os.Mkdir("testdir", 0750)
-		if err != nil && !os.IsExist(err) {
-			log.Fatal(err)
-		}
-		err = os.WriteFile("testdir/testfile.txt", []byte(" eine Texdatei erstellen!"), 0660)
-		if err != nil {
-			log.Fatal(err)
-
-		}
-
-	case "ls":
-		files, err := ioutil.ReadDir("")
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		for _, f := range files {
-			fmt.Println(f.Name())
-		}
-	case "ls-l":
-		out, err := exec.Command("ls", "-l").Output()
-		if err != nil {
-			fmt.Printf("%s", err)
-		}
-		output := string(out[:])
-		fmt.Println(output)
 	}
 
 	cmd := exec.Command(args[0], args[1:]...)
