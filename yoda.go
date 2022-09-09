0cde74f2231b518535ba2305d6443d64064e4a24
womenintech <109299952+womenintech1@users.noreply.github.com>
2022-09-09 12:40:37 +0200

    Update yoda.go

--- yoda.go
+++ yoda.go
@@ -42,6 +42,33 @@ func execInput(input string) error {
 	case "exit":
 		os.Exit(0)
 	}
+	case "mkdir":
+		err := os.Mkdir("testdir", 0750)
+		if err != nil && !os.IsExist(err) {
+			log.Fatal(err)
+		}
+		err = os.WriteFile("testdir/testfile.txt", []byte("Textdatei erstellen"), 0660)
+		if err != nil {
+			log.Fatal(err)
+		}
+	case "ls":
+		files, err := ioutil.ReadDir("./")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, f := range files {
+			fmt.Println(f.Name())
+		}
+	case "ls-l":
+		out, err := exec.Command("ls", "-l").Output()
+		if err != nil {
+			fmt.Printf("%s", err)
+		}
+	
+		output := string(out[:])
+		fmt.Println( output)
+	}
 
 	cmd := exec.Command(args[0], args[1:]...)
 
